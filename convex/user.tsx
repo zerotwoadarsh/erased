47945ce0a63f731555db211d590a282d724b1998
zerotwoadarsh <zerotwoadarsh@gmail.com>
2024-08-20 01:07:36 +0530

    chore: Update Convex generated files

--- convex/user.tsx
+++ convex/user.tsx
@@ -1,5 +1,5 @@
 import {v} from 'convex/values'
-import {query} from './_generated/server'
+import {mutation, query} from './_generated/server'
 
 export const getUser = query({
     args:{
@@ -10,5 +10,16 @@ export const getUser = query({
         const result = await ctx.db.query('user')
         .filter((q) => q.eq(q.field('email'),args.email))
         .collect()
+    }
+})
+
+export const createUser = mutation({
+    args:{
+        name:v.string(),
+        email:v.string(),
+        image:v.string(),
+    },
+    handler:async(ctx: any, args: any)=>{
+       return await ctx.db.insert("user", args)
     }
 })

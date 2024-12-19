d58fedc00c3bfdb0f4867ada40895b3d05e32cd6
zerotwoadarsh <zerotwoadarsh@gmail.com>
2024-12-19 18:58:05 +0530

    feat: Add UserId page and Hero component; enhance CreateTeam page layout and update Header links

--- app/(routes)/teams/create/page.tsx
+++ app/(routes)/teams/create/page.tsx
@@ -4,10 +4,13 @@ import Image from 'next/image'
 function CreateTeam() {
   return (
 
-    <div className='p-16'>
-      <Image src='./public/lolo.svg' alt='logo' width={200} height={200} />
-      <div className=' flex  justify-center'>
-        <h2 className='font-bold text-4xl py-3'>What should we call your team?</h2>
+    <div className='p-16 bg-gradient-to-b from-gray-950 to-gray-900 h-fit'>
+      <Image src='./public/lolo.svg' alt='logo' width={200} height={100} />
+      <div className=' flex flex-col justify-center items-center text-center'>
+        <h2 className='font-bold text-4xl py-3 text-white'>What should we call your team?</h2>
+        <h2 className=' text-gray-400 text-lg'>You can always change this later from settings.</h2>
+        <span className='text-white'>Team Name</span>
+        <input className=' border-2 border-gray-400 rounded-md m-4 p-1 w-2/3 active:border-blue-400'/>
       </div>
     </div>
   )
---
+++ app/_components/Hero.tsx
@@ -0,0 +1,47 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div>
+      <section className=" bg-gradient-to-b from-gray-950 via-zinc-600 to-gray-400  text-white">
+        <div className=' flex items-baseline justify-center pt-16'>
+          <h2 className=' text-white border px-3 p-1 rounded-full text-center border-white'>See more</h2>
+        </div>
+        <div className="mx-auto max-w-screen-xl px-4 py-32 lg:flex lg:h-screen lg:items-center">
+          <div className="mx-auto max-w-3xl text-center">
+            <h1
+              className="bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 bg-clip-text text-3xl font-extrabold text-transparent sm:text-5xl"
+            >
+              Documents & diagrams
+
+              <span className="sm:block"> for engineering teams. </span>
+            </h1>
+
+            <p className="mx-auto mt-8 max-w-xl sm:text-xl/relaxed">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt illo tenetur fuga ducimus
+              numquam ea!
+            </p>
+
+            <div className="mt-8 flex flex-wrap justify-center gap-4">
+              <a
+                className="block w-full rounded border border-blue-600 bg-blue-600 px-12 py-3 text-sm font-medium text-white hover:bg-transparent hover:text-white focus:outline-none focus:ring active:text-opacity-75 sm:w-auto"
+                href="#"
+              >
+                Get Started
+              </a>
+
+              <a
+                className="block w-full rounded border border-blue-600 px-12 py-3 text-sm font-medium text-white hover:bg-blue-600 focus:outline-none focus:ring active:bg-blue-500 sm:w-auto"
+                href="#"
+              >
+                Learn More
+              </a>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  )
+}
+
+export default Hero

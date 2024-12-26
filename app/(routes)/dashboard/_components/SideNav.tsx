248c88021598fcf94c948444f6ed68ac17e4356f
zerotwoadarsh <zerotwoadarsh@gmail.com>
2024-12-26 15:26:16 +0530

    feat: Add SideNav and SideNavTopSection components for improved dashboard navigation

---
+++ app/(routes)/dashboard/_components/SideNav.tsx
@@ -0,0 +1,13 @@
+"use client"  
+import React from 'react'
+import SideNavTopSection from './SideNavTopSection'
+
+const SideNav = () => {
+    return (
+        <div className=' bg-gray-200 h-screen fixed w-64 border-r p-6'>
+            <SideNavTopSection/>
+        </div>
+    )
+}
+
+export default SideNav

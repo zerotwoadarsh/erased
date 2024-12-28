6ab443792d6399d4d832594018f3018106ac5894
zerotwoadarsh <zerotwoadarsh@gmail.com>
2024-12-28 18:25:14 +0530

    feat: Add createTeam mutation to handle team creation functionality

--- convex/teams.tsx
+++ convex/teams.tsx
@@ -1,5 +1,5 @@
 import { v } from 'convex/values'
-import {query} from './_generated/server'
+import {mutation, query} from './_generated/server'
 
 export const getTeam = query({
     args:{email:v.string()},
@@ -8,6 +8,14 @@ export const getTeam = query({
         .filter(q=>q.eq(q.field('createdBy'), args.email))
         .collect();
 
+        return result;
+    }
+})
+
+export const createTeam = mutation({
+    args: {teamName:v.string()},
+    handler: async(ctx, args) =>{
+        const result = await ctx.db.insert('teams', args)
         return result;
     }
 })
